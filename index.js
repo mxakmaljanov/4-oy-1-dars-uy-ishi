eab02f6f9ed803b709e8e6da23169ef5c358a228
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-05-29 17:35:22 +0500

    uy ishi

---
+++ index.js
@@ -0,0 +1,90 @@
+
+//1-masala
+//let L = + prompt("uzunlik");
+//let m = L / 100;
+//m = parseInt(m);
+//document.write (m);
+//2-masala
+//let M = + prompt("kilogramni yozing");
+//let l = M / 1000;
+//l = parseInt(l);
+//document.write (l);
+//3- masala
+//let a = + prompt("baytlarda yozing");
+//let b = a / 1024 ;
+//b = parseInt(b);
+//document.write (b);
+//4-masala
+//let a = + prompt ("a kesma");
+//let b = + prompt ("b kesma");
+//let c = a /b ;
+//c = parseInt(c);
+//document.write (c);
+//6-masala
+//let a = + prompt ("2honali son yozing");
+//let b = a / 10;
+//b = parseInt(b);
+//let c = a % 10;
+//c = parseInt(c)
+//document.write(b);
+//document.write(c);
+//7-masala
+//let a = + prompt ("2 xonali son");
+//let b = a / 10;
+//let c = a % 10;
+//b = parseInt(b);
+//document.write (b+c);
+//8-masala
+//let a = + prompt ("2 xonali son");
+//let b = a / 10;
+//let c = a % 10;
+//b = parseInt(b);
+//document.write (c);
+//document.write (b);
+//9-masala
+//let a = + prompt ("3honali son yozing");
+//let b = a / 100;
+//b = parseInt(b);
+//document.write(b);
+//10-masala
+//let a = + prompt ("3 honali son yozing");
+//let b = a % 100 ;
+//b = parseInt(b); 
+//let c = b % 10 ;
+//c = parseInt(c)
+//let d = b / 10 ;
+//d = parseInt(d);
+//document.write(c);
+//document.write(d);
+//11-masala
+//let a = + prompt ("3 honali son yozing")
+//let b = a % 10;
+//let c = a % 100;
+//let d = c / 10;
+//d = parseInt(d);
+//let e = a / 100;
+//e = parseInt(e);
+//document.write(+d+e);
+//12-masala
+//let a = + prompt ("3 honali sonni kiriting")
+//let b = a % 10;
+//let c = a % 100;
+//let d = c / 10;
+//d = parseInt(d);
+//let e = a / 100;
+//e = parseInt(e);
+//document.write(b);
+//document.write(d);
+//document.write(e);
+//13-masal
+//let a = + prompt ("3 honali sonlarni kiriting")
+//let b = a % 10;
+//let c = a % 100;
+//let d = c / 10;
+//d = parseInt(d);
+//let e = a / 100;
+//e = parseInt(e);
+//document.write(d);
+//document.write(b);
+//document.write(e);
+//4- oy uy ishi 
